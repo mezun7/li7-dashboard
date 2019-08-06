2d9972434696e39ed5ea0b2dfba0d19013bd25fa
Shuhrat Azimuratov <shuhrat94@gmail.com>
2019-08-06 11:54:58 +0300

    quotes and announcements released

--- li7-dashboard/jobs/parse_js.rb
+++ li7-dashboard/jobs/parse_js.rb
@@ -1,22 +1,30 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'net/http'
 require 'uri'
 
 string = '{"desc":{"someKey":"someValue","anotherKey":"value"},"main_item":{"stats":{"a":8,"b":12,"c":10}}}'
+BASE_URL = 'http://127.0.0.1:8000/'
 
 json2 = JSON.parse(string)
-puts json2["desc"]["someKey"]
-
-uri = URI('http://127.0.0.1:8000/front/get_timetable/1/')
+puts json2['desc']['someKey']
+uri = URI("#{BASE_URL}front/get_timetable/1/")
+puts ''
 puts JSON.parse(Net::HTTP.get(uri))['headers'][0]['value']
 
 tmt = {
-    1 => "hi"
+  1 => 'hi'
 }
 
 puts tmt
 
-
 uri2 = URI('http://127.0.0.1:8000/front/announcements/')
 quotes = JSON.parse(Net::HTTP.get(uri2))
-puts quotes['title']
+if !quotes.empty?
+  puts 'empty'
+else
+  uri_quotes = URI("#{BASE_URL}front/quote/")
+  quotes = JSON.parse(Net::HTTP.get(uri_quotes))
+  puts quotes['title']
+end
--- li7-dashboard/jobs/quote.rb
+++ li7-dashboard/jobs/quote.rb
@@ -5,11 +5,20 @@ require 'json'
 require 'net/http'
 require 'uri'
 
-SCHEDULER.every '15s', first_in: 0 do |job|
+SCHEDULER.every '15s', first_in: 0 do |_job|
+  BASE_URL = 'http://127.0.0.1:8000/'
+  url_announcement = "#{BASE_URL}front/announcements/"
+  url_quote = "#{BASE_URL}front/quote/"
+  uri2 = URI(url_announcement)
+  quotes = JSON.parse(Net::HTTP.get(uri2))
 
-	uri2 = URI('http://127.0.0.1:8000/front/announcements/')
-	quotes = JSON.parse(Net::HTTP.get(uri2))
-
-#	send_event('quote', { title: random_quote["author"], text: random_quote["text"], moreinfo: title[random_quote["lang"]] })
-	send_event('quote', title: quotes['title'], text: quotes['text'])
+  #  send_event('quote', { title: random_quote["author"], text: random_quote["text"], moreinfo: title[random_quote["lang"]] })
+  if !quotes.empty?
+    puts 'Here'
+    send_event('quote', title: quotes['title'], text: quotes['text'])
+  else
+    uri3 = URI(url_quote)
+    quotes = JSON.parse(Net::HTTP.get(uri3))
+    send_event('quote', title: quotes['title'], text: quotes['text'])
+  end
 end
