a6de147fae353d08dd15a4c3064c71992db0c14f
Shuhrat Azimuratov <shuhrat94@gmail.com>
2019-08-13 12:17:49 +0300

    Fix substs in the table

--- li7-dashboard/jobs/timetable.rb
+++ li7-dashboard/jobs/timetable.rb
@@ -7,7 +7,7 @@ SCHEDULER.every '1m', first_in: 0 do |_job|
   rows = ''
   time = Time.new
 	wday = time.wday - 1
-  uri = URI("http://127.0.0.1:8000/front/get_timetable/#{wday}/")
+  uri = URI("http://127.0.0.1:8000/front/get_timetable/")
   tmtable = JSON.parse(Net::HTTP.get(uri))
   send_event('timetable', tmtable)
 
