cfd74d765d827287f39a91df1373d7ac67b6f714
Shuhrat Azimuratov <shuhrat94@gmail.com>
2019-08-15 16:47:42 +0300

    Remove legacy in timetable widget

--- li7-dashboard/jobs/timetable.rb
+++ li7-dashboard/jobs/timetable.rb
@@ -4,9 +4,7 @@ require 'json'
 require 'net/http'
 require 'uri'
 SCHEDULER.every '1m', first_in: 0 do |_job|
-  rows = ''
   time = Time.new
-	wday = time.wday - 1
   uri = URI("http://127.0.0.1:8000/front/get_timetable/")
   tmtable = JSON.parse(Net::HTTP.get(uri))
   send_event('timetable', tmtable)
