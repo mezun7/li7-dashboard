c45c7b781f7bd5fee4aade43146dd89e66aa8dd3
Shuhrat Azimuratov <shuhrat94@gmail.com>
2019-08-31 19:29:14 +0300

    Set 1m timeout for dayevents

--- li7-dashboard/jobs/dayevent.rb
+++ li7-dashboard/jobs/dayevent.rb
@@ -3,7 +3,7 @@
 require 'json'
 require 'net/http'
 require 'uri'
-SCHEDULER.every '10s', first_in: 0 do |_job|
+SCHEDULER.every '1m', first_in: 0 do |_job|
   time = Time.new
   uri = URI("http://timetable.litsey7.com/front/dayevents/")
   dayevents = JSON.parse(Net::HTTP.get(uri))
