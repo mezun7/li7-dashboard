adadd168eb9a1885c5080a9a25b60992d7df895f
Shuhrat Azimuratov <shuhrat94@gmail.com>
2019-09-05 11:58:18 +0300

    Set scheduler time of timetable to 15m

--- li7-dashboard/jobs/timetable.rb
+++ li7-dashboard/jobs/timetable.rb
@@ -3,7 +3,7 @@
 require 'json'
 require 'net/http'
 require 'uri'
-SCHEDULER.every '1m', first_in: 0 do |_job|
+SCHEDULER.every '15m', first_in: 0 do |_job|
   time = Time.new
   uri = URI("http://timetable.litsey7.com/front/get_timetable/")
   tmtable = JSON.parse(Net::HTTP.get(uri))
