30aa8428af353c58e7f762eb06ca1302aa4f2c4f
Shuhrat Azimuratov <shuhrat94@gmail.com>
2019-09-19 12:00:19 +0300

    Fix announcements and quotes

--- li7-dashboard/jobs/quote.rb
+++ li7-dashboard/jobs/quote.rb
@@ -4,13 +4,19 @@ require 'json'
 require 'net/http'
 require 'uri'
 
+ind = 0
 SCHEDULER.every '15s', first_in: 0 do |_job|
   BASE_URL = 'http://timetable.litsey7.com'
-  url_announcement = "#{BASE_URL}/front/announcements/"
-  url_quote = "#{BASE_URL}/front/quote/"
+  BASE_URL = 'http://127.0.0.1:8000'
+  url_announcement = "#{BASE_URL}/front/announcements/?ind=#{ind}"
+  url_quote = "#{BASE_URL}/front/quote/?ind=#{ind}"
+
+  p url_announcement
 
   # send_event('quote', { title: random_quote["author"], text: random_quote["text"], moreinfo: title[random_quote["lang"]] })
   quote = JSON.parse(Net::HTTP.get(URI(url_announcement)))
   quote = JSON.parse(Net::HTTP.get(URI(url_quote))) if quote.empty?
   send_event('quote', title: quote['title'], text: quote['text'], icon: quote['icon'])
+  ind += 1
+  ind %= 1000
 end
